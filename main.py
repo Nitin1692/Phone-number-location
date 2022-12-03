26628977ccc5e24e82c4da09b604c8f4c19be53e
Nitin1692 <84765879+Nitin1692@users.noreply.github.com>
2022-12-03 12:49:54 +0530

    Got the country name, latitude and longititude of the phone number

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import phonenumbers
+import opencage
+import folium
 from myphone import number
 
 from phonenumbers import geocoder
 from phonenumbers import carrier
+from opencage.geocoder import OpenCageGeocode
 
 pepnumber = phonenumbers.parse(number)
 location = geocoder.description_for_number(pepnumber, "en")
@@ -10,3 +13,16 @@ print(location)
 
 service_provider = phonenumbers.parse(number)
 print(carrier.name_for_number(service_provider, "en"))
+
+key = 'c3e1216f2e91433c9199273e4cee8a6f'
+
+geocoder = OpenCageGeocode(key)
+query = str(location)
+results = geocoder.geocode(query)
+#print(results)
+
+lat = results[0]['geometry']['lat']
+lng = results[0]['geometry']['lng']
+print(lat,lng)
+
+myMap = folium.map(location=[])
