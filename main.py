36836288b6728ab2767ada72037ba466db3bcf2b
Nitin1692 <84765879+Nitin1692@users.noreply.github.com>
2022-12-03 12:24:06 +0530

    Added pycache and made changes in main.py file

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import phonenumbers
+from myphone import number
+
+from phonenumbers import geocoder
+from phonenumbers import carrier
+
+pepnumber = phonenumbers.parse(number)
+location = geocoder.description_for_number(pepnumber, "en")
+print(location)
+
+service_provider = phonenumbers.parse(number)
+print(carrier.name_for_number(service_provider, "en"))
